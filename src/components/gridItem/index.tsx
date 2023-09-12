0e70b25fd4814c84719e45abf9a9494799eef48d
guilopes2020 <guilopesdev@gmail.com>
2023-09-12 18:06:51 -0300

    feat: :sparkles: working-in-the-system

--- src/components/gridItem/index.tsx
+++ src/components/gridItem/index.tsx
@@ -14,6 +14,11 @@ export const GridItem = ({ item }: Props) => {
                 <img src={item.icon === 'up' ? upImage : dowImage} alt="" width='30' />
             </div>
             <div className={styles.gridTitle}>{item.title}</div>
+
+            {item.yourImc &&
+                <div className={styles.yourImc}>Seu Imc é de {item.yourImc} Kg/m2</div>
+            }
+
             <div className={styles.gridInfo}>
                 <>
                     IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
